4efe366d13210dd1a622b1e3948094ee239a5a30
leep-frog <gleeper49@gmail.com>
2022-01-01 11:57:28 -0500

    gc generator with math ints

--- generator/generator.go
+++ generator/generator.go
@@ -1,5 +1,35 @@
 package generator
 
+import "github.com/leep-frog/euler_challenge/maths"
+
+// TODO: Use go1.18 parameters to implement this with int and maths.Int
+type IntGenerator struct {
+	values []*maths.Int
+
+	f func(*IntGenerator) *maths.Int
+}
+
+func (ig *IntGenerator) Last() *maths.Int {
+	return ig.values[len(ig.values)-1]
+}
+
+func (ig *IntGenerator) Len() int {
+	return len(ig.values)
+}
+
+func (ig *IntGenerator) Nth(i int) *maths.Int {
+	for len(ig.values) <= i {
+		ig.Next()
+	}
+	return ig.values[i]
+}
+
+func (ig *IntGenerator) Next() *maths.Int {
+	i := ig.f(ig)
+	ig.values = append(ig.values, i)
+	return i
+}
+
 type Generator struct {
 	values []int
 
@@ -38,12 +68,40 @@ func NewGenerator(start int, f func(*Generator) int) *Generator {
 	}
 }
 
+func NewIntGenerator(start *maths.Int, f func(*IntGenerator) *maths.Int) *IntGenerator {
+	return &IntGenerator{
+		f: func(g *IntGenerator) *maths.Int {
+			if len(g.values) == 0 {
+				return start
+			}
+			return f(g)
+		},
+	}
+}
+
 func Primes() *Generator {
 	return NewGenerator(2, func(g *Generator) int {
 		for i := g.Last() + 1; ; i++ {
 			newPrime := true
 			for _, p := range g.values {
 				if i%p == 0 {
+					newPrime = false
+					break
+				}
+			}
+			if newPrime {
+				return i
+			}
+		}
+	})
+}
+
+func PrimesInt() *IntGenerator {
+	return NewIntGenerator(maths.NewInt(2), func(g *IntGenerator) *maths.Int {
+		for i := g.Last().Plus(maths.One()); ; i.PP() {
+			newPrime := true
+			for _, p := range g.values {
+				if _, rem := i.Div(p); rem.EQ(maths.Zero()) {
 					newPrime = false
 					break
 				}
@@ -66,10 +124,29 @@ func Fibonaccis() *Generator {
 	})
 }
 
+func FibonaccisInt() *IntGenerator {
+	a := maths.NewInt(1)
+	b := maths.NewInt(2)
+	return NewIntGenerator(maths.NewInt(1), func(ig *IntGenerator) *maths.Int {
+		r := b
+		b = a.Plus(b)
+		a = r
+		return a
+	})
+}
+
 func Triangulars() *Generator {
 	i := 1
 	return NewGenerator(1, func(g *Generator) int {
 		i++
 		return g.Last() + i
+	})
+}
+
+func TriangularsInt() *IntGenerator {
+	i := maths.One()
+	return NewIntGenerator(maths.One(), func(ig *IntGenerator) *maths.Int {
+		i.PP()
+		return ig.Last().Plus(i)
 	})
 }
--- generator/generator_test.go
+++ generator/generator_test.go
@@ -4,17 +4,20 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/leep-frog/euler_challenge/maths"
 )
 
 func TestGenerators(t *testing.T) {
 	for _, test := range []struct {
 		name string
 		g    *Generator
+		ig   *IntGenerator
 		want []int
 	}{
 		{
 			name: "Generates primes",
 			g:    Primes(),
+			ig:   PrimesInt(),
 			want: []int{
 				2, 3, 5, 7, 11, 13,
 			},
@@ -22,6 +25,7 @@ func TestGenerators(t *testing.T) {
 		{
 			name: "Generates triangulars",
 			g:    Triangulars(),
+			ig:   TriangularsInt(),
 			want: []int{
 				1, 3, 6, 10, 15, 21,
 			},
@@ -29,6 +33,7 @@ func TestGenerators(t *testing.T) {
 		{
 			name: "Generates fibonaccis",
 			g:    Fibonaccis(),
+			ig:   FibonaccisInt(),
 			want: []int{
 				1, 2, 3, 5, 8, 13, 21,
 			},
@@ -40,10 +45,20 @@ func TestGenerators(t *testing.T) {
 			}
 
 			var nexts, lasts, nths []int
+			var nextsI, lastsI, nthsI []*maths.Int
 			for i := range test.want {
 				nexts = append(nexts, test.g.Next())
 				lasts = append(lasts, test.g.Last())
 				nths = append(nths, test.g.Nth(i))
+
+				nextsI = append(nextsI, test.ig.Next())
+				lastsI = append(lastsI, test.ig.Last())
+				nthsI = append(nthsI, test.ig.Nth(i))
+			}
+
+			var wantI []*maths.Int
+			for _, w := range test.want {
+				wantI = append(wantI, maths.NewInt(int64(w)))
 			}
 
 			if diff := cmp.Diff(test.want, nexts); diff != "" {
@@ -56,6 +71,19 @@ func TestGenerators(t *testing.T) {
 
 			if diff := cmp.Diff(test.want, nths); diff != "" {
 				t.Errorf("Generator.Nth() returned incorrect values (-want, +got):\n%s", diff)
+			}
+
+			// IntGenerator
+			if diff := cmp.Diff(wantI, nextsI, maths.CmpOpts()...); diff != "" {
+				t.Errorf("IntGenerator.Next() returned incorrect values (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(wantI, lastsI, maths.CmpOpts()...); diff != "" {
+				t.Errorf("IntGenerator.Last() returned incorrect values (-want, +got):\n%s", diff)
+			}
+
+			if diff := cmp.Diff(wantI, nthsI, maths.CmpOpts()...); diff != "" {
+				t.Errorf("IntGenerator.Nth() returned incorrect values (-want, +got):\n%s", diff)
 			}
 		})
 	}
