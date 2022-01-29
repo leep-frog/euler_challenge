f1c6f910b1dab07929eeeb2dd3b715bb4d7b6b8b
leep-frog <gleeper49@gmail.com>
2022-01-28 23:14:00 -0500

    p80

--- ec/p80_square_root_digital_expansion.go
+++ ec/p80_square_root_digital_expansion.go
@@ -1,11 +1,37 @@
 package eulerchallenge
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
+	"github.com/leep-frog/euler_challenge/maths"
 )
 
 func P80() *problem {
 	return noInputNode(80, func(o command.Output) {
-
+		bigNum := maths.MustIntFromString("1" + strings.Repeat("0", 100))
+		var sum int
+		for n := 2; n <= 100; n++ {
+			if maths.IsSquare(n) {
+				continue
+			}
+			start, period := maths.SquareRootPeriod(n)
+			den := maths.One()
+			startIdx := 200
+			num := maths.NewInt(int64(period[startIdx%len(period)]))
+			for idx := startIdx - 1; idx >= 0; idx-- {
+				tmp := den
+				den = num
+				num = tmp.Plus(den.Times(maths.NewInt(int64(period[idx%len(period)]))))
+			}
+			tmp := den
+			den = num
+			num = tmp.Plus(den.Times(maths.NewInt(int64(start))))
+			//remainder := num.Minus(den.Times(maths.NewInt(int64(start))))
+			remainder := num
+			digits := maths.MustIntFromString(remainder.Times(bigNum).Div(den).String()[:100])
+			sum += digits.DigitSum()
+		}
+		o.Stdoutln(sum)
 	})
 }
