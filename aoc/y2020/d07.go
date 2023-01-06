de2558f3e4b03b58f5edfae383a90ac3765bd863
leep-frog <gleeper49@gmail.com>
2023-01-05 19:10:38 -0500

    aoc 2020 d08

--- aoc/y2020/d07.go
+++ aoc/y2020/d07.go
@@ -1,8 +1,13 @@
 package y2020
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day07() aoc.Day {
@@ -11,7 +16,64 @@ func Day07() aoc.Day {
 
 type day07 struct{}
 
+func (d *day07) explore(bag string, paths map[string][]string, checked map[string]bool) {
+	if checked[bag] {
+		return
+	}
+	checked[bag] = true
+	for _, parentBag := range paths[bag] {
+		d.explore(parentBag, paths, checked)
+	}
+}
+
+func (d *day07) explore2(bag string, paths map[string][]*bagCount, checked map[string]int) int {
+	if v, ok := checked[bag]; ok {
+		return v
+	}
+
+	var sum int
+	for _, bc := range paths[bag] {
+		sum += bc.count * (1 + d.explore2(bc.bag, paths, checked))
+	}
+	checked[bag] = sum
+	return sum
+}
+
+type bagCount struct {
+	bag   string
+	count int
+}
+
+func (bc *bagCount) String() string {
+	return fmt.Sprintf("[%d %v]", bc.count, bc.bag)
+}
+
 func (d *day07) Solve(lines []string, o command.Output) {
+	paths := map[string][]string{}
+	revPaths := map[string][]*bagCount{}
+	r := regexp.MustCompile(`^([a-z ]*) bags contain(.*)\.$`)
+	sr := regexp.MustCompile(`^ ([0-9]+) ([a-z ]*) bags?$`)
+	for _, line := range lines {
+		m := r.FindStringSubmatch(line)
+		bag := m[1]
+		if m[2] == " no other bags" {
+			continue
+		}
+		for _, part := range strings.Split(m[2], ",") {
+			sm := sr.FindStringSubmatch(part)
+			cnt := parse.Atoi(sm[1])
+			subbag := sm[2]
+
+			paths[subbag] = append(paths[subbag], bag)
+			revPaths[bag] = append(revPaths[bag], &bagCount{subbag, cnt})
+		}
+	}
+
+	containable := map[string]bool{}
+	d.explore("shiny gold", paths, containable)
+	fmt.Println(revPaths)
+	o.Stdoutln(d.explore2("shiny gold", revPaths, map[string]int{}))
+	fmt.Println(len(containable))
 }
 
 func (d *day07) Cases() []*aoc.Case {
--- aoc/y2020/d08.go
+++ aoc/y2020/d08.go
@@ -1,8 +1,11 @@
 package y2020
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day08() aoc.Day {
@@ -11,7 +14,67 @@ func Day08() aoc.Day {
 
 type day08 struct{}
 
+type instruction struct {
+	code  string
+	value int
+}
+
+func (d *day08) solve1(instructions []*instruction) (int, bool) {
+	run := make([]bool, len(instructions), len(instructions))
+	acc := 0
+
+	i := 0
+	for i < len(instructions) && !run[i] {
+		run[i] = true
+		instr := instructions[i]
+		switch instr.code {
+		case "nop":
+			i++
+		case "acc":
+			acc += instr.value
+			i++
+		case "jmp":
+			i += instr.value
+		}
+	}
+	return acc, i == len(instructions)
+}
+
+func (d *day08) solve2(instructions []*instruction) int {
+	for _, instr := range instructions {
+		switch instr.code {
+		case "nop":
+			instr.code = "jmp"
+			v, ok := d.solve1(instructions)
+			instr.code = "nop"
+			if ok {
+				return v
+			}
+		case "jmp":
+			instr.code = "nop"
+			v, ok := d.solve1(instructions)
+			instr.code = "jmp"
+			if ok {
+				return v
+			}
+		}
+	}
+	return -1
+}
+
 func (d *day08) Solve(lines []string, o command.Output) {
+	var run []bool
+	var instructions []*instruction
+	for _, line := range lines {
+		run = append(run, false)
+		instructions = append(instructions, &instruction{
+			line[:3],
+			parse.Atoi(strings.Replace(line[4:], "+", "", -1)),
+		})
+	}
+
+	v, _ := d.solve1(instructions)
+	o.Stdoutln(v, d.solve2(instructions))
 }
 
 func (d *day08) Cases() []*aoc.Case {
@@ -19,12 +82,12 @@ func (d *day08) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"5 8",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"1928 1319",
 			},
 		},
 	}
