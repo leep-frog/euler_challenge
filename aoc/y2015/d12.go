5a6172c3b7d9f9010b67594c8323a165ac23ef39
leep-frog <gleeper49@gmail.com>
2023-01-25 22:11:46 -0500

    aoc 2015 d13

--- aoc/y2015/d12.go
+++ aoc/y2015/d12.go
@@ -1,6 +1,9 @@
 package y2015
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
 )
@@ -11,7 +14,42 @@ func Day12() aoc.Day {
 
 type day12 struct{}
 
+func (d *day12) search(k interface{}, sum *int, part2 bool) {
+	switch v := k.(type) {
+	case int:
+		*sum += v
+	case []interface{}:
+		for _, inf := range v {
+			d.search(inf, sum, part2)
+		}
+	case map[string]interface{}:
+		if part2 {
+			for _, val := range v {
+				if val == "red" {
+					return
+				}
+			}
+		}
+		for _, inf := range v {
+			d.search(inf, sum, part2)
+		}
+	case string:
+	case float64:
+		*sum += int(v)
+	case bool:
+	default:
+		fmt.Printf("UNKONWN TYPE: %t", v)
+	}
+}
+
 func (d *day12) Solve(lines []string, o command.Output) {
+	var result []interface{}
+	var sum1, sum2 int
+	json.Unmarshal([]byte(lines[0]), &result)
+	d.search(result, &sum1, false)
+	d.search(result, &sum2, true)
+
+	o.Stdoutln(sum1, sum2)
 }
 
 func (d *day12) Cases() []*aoc.Case {
@@ -19,12 +57,12 @@ func (d *day12) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"6 4",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"191164 87842",
 			},
 		},
 	}
--- aoc/y2015/d13.go
+++ aoc/y2015/d13.go
@@ -1,8 +1,13 @@
 package y2015
 
 import (
+	"regexp"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
+	"github.com/leep-frog/euler_challenge/maths"
+	"github.com/leep-frog/euler_challenge/parse"
+	"golang.org/x/exp/maps"
 )
 
 func Day13() aoc.Day {
@@ -11,7 +16,46 @@ func Day13() aoc.Day {
 
 type day13 struct{}
 
+func (d *day13) rec(startingPerson, currentPerson string, utils map[string]map[string]int, people map[string]bool, total int, best *maths.Bester[int, int]) {
+	if len(people) == 1 {
+		best.Check(total + utils[startingPerson][currentPerson] + utils[currentPerson][startingPerson])
+		return
+	}
+
+	delete(people, currentPerson)
+	for _, nextPerson := range maps.Keys(people) {
+		d.rec(startingPerson, nextPerson, utils, people, total+utils[currentPerson][nextPerson]+utils[nextPerson][currentPerson], best)
+	}
+	people[currentPerson] = true
+}
+
 func (d *day13) Solve(lines []string, o command.Output) {
+	o.Stdoutln(d.solve(lines, false), d.solve(lines, true))
+}
+
+func (d *day13) solve(lines []string, part2 bool) int {
+	utils := map[string]map[string]int{}
+	people := map[string]bool{}
+	r := regexp.MustCompile(`^(.*) would (.*) ([0-9]+) happiness units by sitting next to (.*)\.$`)
+	for _, line := range lines {
+		m := r.FindStringSubmatch(line)
+		a, u, b := m[1], parse.Atoi(m[3]), m[4]
+		if m[2] == "lose" {
+			u = -u
+		}
+		maths.Insert(utils, a, b, u)
+		people[a] = true
+	}
+
+	if part2 {
+		people["you"] = true
+		utils["you"] = map[string]int{}
+	}
+
+	best := maths.Largest[int, int]()
+	delete(people, "Alice")
+	d.rec("Alice", "Alice", utils, people, 0, best)
+	return best.Best()
 }
 
 func (d *day13) Cases() []*aoc.Case {
@@ -19,12 +63,12 @@ func (d *day13) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"330 286",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"709 668",
 			},
 		},
 	}
