ee430c80cfa7dc7752b57328ac0dec4d7ecd64a7
leep-frog <gleeper49@gmail.com>
2023-01-25 21:26:33 -0500

    aoc 2015 d10

--- aoc/y2015/d09.go
+++ aoc/y2015/d09.go
@@ -1,8 +1,13 @@
 package y2015
 
 import (
+	"regexp"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
+	"github.com/leep-frog/euler_challenge/maths"
+	"github.com/leep-frog/euler_challenge/parse"
+	"golang.org/x/exp/maps"
 )
 
 func Day09() aoc.Day {
@@ -11,7 +16,40 @@ func Day09() aoc.Day {
 
 type day09 struct{}
 
+func (d *day09) rec(distances map[string]map[string]int, city string, remaining map[string]bool, dist int, best *maths.Bester[int, int]) {
+	if len(remaining) == 1 {
+		best.Check(dist)
+		return
+	}
+
+	delete(remaining, city)
+	for _, toCity := range maps.Keys(remaining) {
+		d.rec(distances, toCity, remaining, dist+distances[city][toCity], best)
+	}
+	remaining[city] = true
+}
+
 func (d *day09) Solve(lines []string, o command.Output) {
+	var parts []int
+	for _, best := range []*maths.Bester[int, int]{maths.Smallest[int, int](), maths.Largest[int, int]()} {
+		cities := map[string]map[string]int{}
+		remaining := map[string]bool{}
+		r := regexp.MustCompile(`^(.*) to (.*) = (.*)$`)
+		for _, line := range lines {
+			m := r.FindStringSubmatch(line)
+			ca, cb, dist := m[1], m[2], parse.Atoi(m[3])
+			remaining[ca] = true
+			remaining[cb] = true
+			maths.Insert(cities, ca, cb, dist)
+			maths.Insert(cities, cb, ca, dist)
+		}
+
+		for city := range cities {
+			d.rec(cities, city, remaining, 0, best)
+		}
+		parts = append(parts, best.Best())
+	}
+	o.Stdoutln(parts[0], parts[1])
 }
 
 func (d *day09) Cases() []*aoc.Case {
@@ -19,12 +57,12 @@ func (d *day09) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"605 982",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"117 909",
 			},
 		},
 	}
--- aoc/y2015/d10.go
+++ aoc/y2015/d10.go
@@ -1,8 +1,11 @@
 package y2015
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day10() aoc.Day {
@@ -11,7 +14,39 @@ func Day10() aoc.Day {
 
 type day10 struct{}
 
+func (d *day10) process(code string) string {
+	var r []string
+	var prevChar string
+	var charCount int
+	for idx, c := range strings.Split(code, "") {
+		if idx == 0 {
+			prevChar = c
+			charCount++
+			continue
+		}
+
+		if c == prevChar {
+			charCount++
+		} else {
+			r = append(r, parse.Itos(charCount), prevChar)
+			prevChar = c
+			charCount = 1
+		}
+	}
+	r = append(r, parse.Itos(charCount), prevChar)
+	return strings.Join(r, "")
+}
+
 func (d *day10) Solve(lines []string, o command.Output) {
+	code := lines[0]
+	var part1 int
+	for i := 0; i < 50; i++ {
+		if i == 40 {
+			part1 = len(code)
+		}
+		code = d.process(code)
+	}
+	o.Stdoutln(part1, len(code))
 }
 
 func (d *day10) Cases() []*aoc.Case {
@@ -19,12 +54,12 @@ func (d *day10) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"82350 1166642",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"252594 3579328",
 			},
 		},
 	}
