85a8b57775083c5576edb1c7cc50922cf271ede0
leep-frog <gleeper49@gmail.com>
2023-02-01 10:27:28 -0500

    update command package

--- aoc/y2015/d19.go
+++ aoc/y2015/d19.go
@@ -8,8 +8,6 @@ import (
 	"github.com/leep-frog/euler_challenge/aoc/aoc"
 	"github.com/leep-frog/euler_challenge/bfs"
 	"github.com/leep-frog/euler_challenge/maths"
-	"golang.org/x/exp/maps"
-	"golang.org/x/exp/slices"
 )
 
 func Day19() aoc.Day {
@@ -27,31 +25,29 @@ func (m *molecule) Code(mc *moleculeContext) string {
 	return fmt.Sprintf("%s %d", m.m, m.depth)
 }
 
-func (m *molecule) Distance(mc *moleculeContext) bfs.Int {
+/*func (m *molecule) Distance(mc *moleculeContext) bfs.Int {
 	return bfs.Int(len(m.m))
-}
+}*/
 
 var (
-	yup = 1_000
+	dpth = 0
 )
 
 func (m *molecule) Done(mc *moleculeContext) bool {
-	// fmt.Println("C", m.m)
-	if len(m.m) < yup {
-		fmt.Println()
-		fmt.Println("BEST:", len(m.m))
-		fmt.Println()
-		yup = len(m.m)
+	if m.depth > dpth {
+		dpth = m.depth
+		fmt.Println("DEPTH", dpth)
 	}
-	fmt.Println(m.m)
-	if m.m == "e" {
-		mc.best.Check(m.depth)
-	}
-	return false
+	return m.m == "e"
 }
 
 func (m *molecule) AdjacentStates(mc *moleculeContext) []*molecule {
-	if mc.best.Set() && m.depth >= mc.best.Best() {
+	var r []*molecule
+	for t := range mc.transformations(m.m) {
+		r = append(r, &molecule{t, m.depth + 1})
+	}
+	return r
+	/*if mc.best.Set() && m.depth >= mc.best.Best() {
 		return nil
 	}
 
@@ -63,7 +59,7 @@ func (m *molecule) AdjacentStates(mc *moleculeContext) []*molecule {
 	for k := range trans {
 		r = append(r, &molecule{k, m.depth + 1})
 	}
-	return r
+	return r*/
 }
 
 func (d *day19) rec2(oneof map[string]bool, mc *moleculeContext, mol string, depth int, cache map[string]int, best *maths.Bester[int, int]) int {
@@ -108,7 +104,23 @@ func (d *day19) rec(mc *moleculeContext, mol string, depth int, cache map[string
 	// cache[mol] = []int{}
 }
 
+func (d *day19) solve2(depth int, mol string, mc *moleculeContext, leftOfRn map[string]bool, rightOfRn map[string]bool, best *maths.Bester[int, int]) {
+	parts := strings.SplitN(mol, "Rn", 2)
+
+	if len(parts) == 1 {
+		_, dist := bfs.ContextSearch[*moleculeContext, string](mc, []*molecule{&molecule{mol, 0}})
+		best.Check(dist)
+		return
+	}
+
+	leftOps
+	for opt, dist := range mc.Explore(mol, leftOfRn) {
+
+	}
+}
+
 func (d *day19) Solve(lines []string, o command.Output) {
+	fmt.Println("START")
 	ops := map[string][]string{}
 	revOps := map[string][]string{}
 	var maxLen, revMaxLen int
@@ -124,13 +136,29 @@ func (d *day19) Solve(lines []string, o command.Output) {
 	revMC := &moleculeContext{revOps, revMaxLen, "e", maths.Smallest[int, int]()}
 
 	part1 := len(mc.transformations(mol))
+	fmt.Println("UNO", part1)
 
-	// bfs.ContextDistanceSearch[*moleculeContext, string, bfs.Int](revMC, []*molecule{{mol, 0}})
-	o.Stdoutln(part1, revMC.best.Best())
+	leftOfRnArr := map[string][]string{}
+	leftOfRnBool := map[string]bool{}
+	for conv := range mc.ops {
+		if parts := strings.Split(conv, "Rn"); len(parts) > 1 {
+			leftOfRnArr[parts[0]] = append(leftOfRnArr[parts[0]], parts[1])
+			leftOfRnBool[parts[0]] = true
+		}
+	}
 
-	b := maths.Smallest[int, int]()
-	d.rec(revMC, mol, 0, map[string]int{}, b)
-	fmt.Println(b)
+	best := maths.Smallest[int, int]()
+	d.solve2(0, mol, revMC, leftOfRnBool, best)
+	fmt.Println(best)
+
+	/*
+		path, dist := bfs.ContextSearch[*moleculeContext, string](revMC, []*molecule{{mol, 0}})
+		fmt.Println(dist, path)*/
+	// o.Stdoutln(part1, revMC.best.Best())
+
+	// b := maths.Smallest[int, int]()
+	// d.rec(revMC, mol, 0, map[string]int{}, b)
+	// fmt.Println(b)
 }
 
 type moleculeContext struct {
@@ -138,6 +166,26 @@ type moleculeContext struct {
 	maxLen int
 	target string
 	best   *maths.Bester[int, int]
+}
+
+func (mc *moleculeContext) Explore(molecule string, valid map[string]bool) map[string]int {
+	r := map[string]int{}
+	mc.explore(molecule, valid, r, 0)
+	return r
+}
+
+func (mc *moleculeContext) explore(molecule string, valid map[string]bool, values map[string]int, depth int) {
+	if valid[molecule] {
+		if v, ok := values[molecule]; ok {
+			values[molecule] = maths.Min(v, depth)
+		} else {
+			values[molecule] = depth
+		}
+	}
+
+	for t := range mc.transformations(molecule) {
+		mc.explore(t, valid, values, depth+1)
+	}
 }
 
 func (mc *moleculeContext) transformations(molecule string) map[string]bool {
