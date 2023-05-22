3cfb7c95eea4506076ae74d14dc4d38b4da20386
leep-frog <gleeper49@gmail.com>
2023-05-21 21:29:54 -0400

    2016 18

--- aoc/y2016/d17.go
+++ aoc/y2016/d17.go
@@ -3,6 +3,7 @@ package y2016
 import (
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/maths"
 )
 
 func Day17() aoc.Day {
@@ -12,6 +13,61 @@ func Day17() aoc.Day {
 type day17 struct{}
 
 func (d *day17) Solve(lines []string, o command.Output) {
+	passcode := lines[0]
+
+	shortest, longest := maths.Smallest[string, int](), maths.Largest[string, int]()
+	start := &room{0, 0, passcode}
+	start.dfs(0, shortest, longest)
+
+	bestPath := shortest.BestIndex()[len(passcode):]
+	o.Stdoutln(bestPath, longest.Best())
+}
+
+type room struct {
+	x, y int
+	path string
+}
+
+var (
+	validHashes = map[rune]bool{
+		'b': true,
+		'c': true,
+		'd': true,
+		'e': true,
+		'f': true,
+	}
+)
+
+func (r *room) dfs(depth int, shortest, longest *maths.Bester[string, int]) {
+	// Check if done
+	if r.x == 3 && r.y == 3 {
+		shortest.IndexCheck(r.path, depth)
+		longest.IndexCheck(r.path, depth)
+		return
+	}
+
+	hash := md5Hash(r.path, 1)[:4]
+
+	// Up
+	if r.y > 0 && validHashes[rune(hash[0])] {
+		newR := &room{r.x, r.y - 1, r.path + "U"}
+		newR.dfs(depth+1, shortest, longest)
+	}
+
+	if r.y < 3 && validHashes[rune(hash[1])] {
+		newR := &room{r.x, r.y + 1, r.path + "D"}
+		newR.dfs(depth+1, shortest, longest)
+	}
+
+	if r.x > 0 && validHashes[rune(hash[2])] {
+		newR := &room{r.x - 1, r.y, r.path + "L"}
+		newR.dfs(depth+1, shortest, longest)
+	}
+
+	if r.x < 3 && validHashes[rune(hash[3])] {
+		newR := &room{r.x + 1, r.y, r.path + "R"}
+		newR.dfs(depth+1, shortest, longest)
+	}
 }
 
 func (d *day17) Cases() []*aoc.Case {
@@ -19,12 +75,12 @@ func (d *day17) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"DDUDRLRRUDRD 492",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"590 RLDUDRDDRR",
 			},
 		},
 	}
--- aoc/y2016/d18.go
+++ aoc/y2016/d18.go
@@ -1,8 +1,11 @@
 package y2016
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day18() aoc.Day {
@@ -11,7 +14,49 @@ func Day18() aoc.Day {
 
 type day18 struct{}
 
+func (d *day18) toString(traps []bool) string {
+	var r []string
+	for _, t := range traps {
+		if t {
+			r = append(r, "^")
+		} else {
+			r = append(r, ".")
+		}
+	}
+	return strings.Join(r, "")
+}
+
 func (d *day18) Solve(lines []string, o command.Output) {
+	o.Stdoutln(d.solve(lines[0], parse.Atoi(lines[1])), d.solve(lines[0], parse.Atoi(lines[2])))
+}
+
+func (d *day18) solve(start string, rows int) int {
+	var safeCount int
+	var traps []bool
+	for _, c := range start {
+		traps = append(traps, c == '^')
+		if c != '^' {
+			safeCount++
+		}
+	}
+
+	for i := 0; i < rows-1; i++ {
+		var nt []bool
+		for ti, t := range traps {
+			left := ti > 0 && traps[ti-1]
+			middle := t
+			right := ti < len(traps)-1 && traps[ti+1]
+
+			isTrap := (left && middle && !right) || (!left && middle && right) || (left && !middle && !right) || (!left && !middle && right)
+			nt = append(nt, isTrap)
+
+			if !isTrap {
+				safeCount++
+			}
+		}
+		traps = nt
+	}
+	return safeCount
 }
 
 func (d *day18) Cases() []*aoc.Case {
@@ -19,12 +64,12 @@ func (d *day18) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"38 38",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"2005 20008491",
 			},
 		},
 	}
