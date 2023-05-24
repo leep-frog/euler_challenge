5aeaaff635676e1e2f7a1ce65d058c4b9b1e8c22
leep-frog <gleeper49@gmail.com>
2023-05-24 19:37:13 -0400

    2017 8

--- aoc/y2017/d08.go
+++ aoc/y2017/d08.go
@@ -1,8 +1,13 @@
 package y2017
 
 import (
+	"fmt"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/maths"
+	"github.com/leep-frog/euler_challenge/parse"
+	"golang.org/x/exp/maps"
 )
 
 func Day08() aoc.Day {
@@ -12,6 +17,38 @@ func Day08() aoc.Day {
 type day08 struct{}
 
 func (d *day08) Solve(lines []string, o command.Output) {
+	registers := map[string]int{}
+	max := maths.Largest[int, int]()
+	for _, parts := range parse.SplitWhitespace(lines) {
+		left, right := registers[parts[4]], parse.Atoi(parts[6])
+		var yes bool
+		switch parts[5] {
+		case "==":
+			yes = left == right
+		case "!=":
+			yes = left != right
+		case ">":
+			yes = left > right
+		case "<":
+			yes = left < right
+		case ">=":
+			yes = left >= right
+		case "<=":
+			yes = left <= right
+		default:
+			panic(fmt.Sprintf("Unknown op: %q", parts[5]))
+		}
+		if yes {
+			switch parts[1] {
+			case "inc":
+				registers[parts[0]] += parse.Atoi(parts[2])
+			case "dec":
+				registers[parts[0]] -= parse.Atoi(parts[2])
+			}
+			max.Check(registers[parts[0]])
+		}
+	}
+	o.Stdoutln(maths.Max(maps.Values(registers)...), max.Best())
 }
 
 func (d *day08) Cases() []*aoc.Case {
