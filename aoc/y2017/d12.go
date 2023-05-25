c5d0e591ce50fb4ba0d5b211e0c073a24a405547
leep-frog <gleeper49@gmail.com>
2023-05-25 09:26:39 -0400

    2017 13

--- aoc/y2017/d12.go
+++ aoc/y2017/d12.go
@@ -1,8 +1,13 @@
 package y2017
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/functional"
+	"github.com/leep-frog/euler_challenge/parse"
+	"github.com/leep-frog/euler_challenge/unionfind"
 )
 
 func Day12() aoc.Day {
@@ -12,6 +17,19 @@ func Day12() aoc.Day {
 type day12 struct{}
 
 func (d *day12) Solve(lines []string, o command.Output) {
+	uf := unionfind.New()
+
+	for _, line := range lines {
+		sides := strings.Split(line, " <-> ")
+		left := parse.Atoi(sides[0])
+		for _, right := range strings.Split(sides[1], ", ") {
+			uf.Merge(left, parse.Atoi(right))
+		}
+	}
+
+	o.Stdoutln(functional.CountFunc(uf.Elements(), func(k int) bool {
+		return uf.Connected(0, k)
+	}), uf.NumberOfSets())
 }
 
 func (d *day12) Cases() []*aoc.Case {
@@ -19,12 +37,12 @@ func (d *day12) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"6 2",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"134 193",
 			},
 		},
 	}
--- aoc/y2017/d13.go
+++ aoc/y2017/d13.go
@@ -1,8 +1,12 @@
 package y2017
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/functional"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day13() aoc.Day {
@@ -11,7 +15,38 @@ func Day13() aoc.Day {
 
 type day13 struct{}
 
+type depthRange struct {
+	d  int
+	r  int
+	dr int
+}
+
 func (d *day13) Solve(lines []string, o command.Output) {
+	drs := functional.Map(lines, func(line string) *depthRange {
+		parts := strings.Split(line, ": ")
+		d, r := parse.Atoi(parts[0]), parse.Atoi(parts[1])
+		return &depthRange{d, r, d * r}
+	})
+
+	part1, _ := d.severity(0, drs)
+	for ps := 0; ; ps++ {
+		if _, ok := d.severity(ps, drs); ok {
+			o.Stdoutln(part1, ps)
+			return
+		}
+	}
+}
+
+func (d *day13) severity(ps int, drs []*depthRange) (int, bool) {
+	var severity int
+	ok := true
+	for _, dr := range drs {
+		if (ps+dr.d)%((dr.r-1)*2) == 0 {
+			severity += dr.dr
+			ok = false
+		}
+	}
+	return severity, ok
 }
 
 func (d *day13) Cases() []*aoc.Case {
@@ -19,12 +54,12 @@ func (d *day13) Cases() []*aoc.Case {
 		{
 			FileSuffix: "example",
 			ExpectedOutput: []string{
-				"",
+				"24 10",
 			},
 		},
 		{
 			ExpectedOutput: []string{
-				"",
+				"1504 3823370",
 			},
 		},
 	}
