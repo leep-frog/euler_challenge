9cd3002e8231e315529c3417b50d4bb1a32ee46e
leep-frog <gleeper49@gmail.com>
2023-05-25 13:16:36 -0400

    2017 15

--- aoc/y2017/d15.go
+++ aoc/y2017/d15.go
@@ -1,8 +1,11 @@
 package y2017
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/parse"
 )
 
 func Day15() aoc.Day {
@@ -11,7 +14,42 @@ func Day15() aoc.Day {
 
 type day15 struct{}
 
+type generator struct {
+	value  int
+	factor int
+	rem    int
+	divBy  int
+}
+
+func (g *generator) Next() int {
+	g.value = (g.value * g.factor) % g.rem
+	for (g.value % g.divBy) != 0 {
+		g.value = (g.value * g.factor) % g.rem
+	}
+	return g.value
+}
+
+func (d *day15) solve(aStart, aDiv, bStart, bDiv, times int) int {
+	rem := 2147483647
+	a, b := &generator{aStart, 16807, rem, aDiv}, &generator{bStart, 48271, rem, bDiv}
+	var count int
+	for i := 0; i < times; i++ {
+		av, bv := a.Next(), b.Next()
+		if av%65536 == bv%65536 {
+			count++
+		}
+	}
+	return count
+}
+
 func (d *day15) Solve(lines []string, o command.Output) {
+
+	// startA, startB := 65, 8921
+	// startA, startB := 783, 325
+	parts := strings.Split(lines[0], ",")
+	startA, startB := parse.Atoi(parts[0]), parse.Atoi(parts[1])
+
+	o.Stdoutln(d.solve(startA, 1, startB, 1, 40_000_000), d.solve(startA, 4, startB, 8, 5_000_000))
 }
 
 func (d *day15) Cases() []*aoc.Case {
