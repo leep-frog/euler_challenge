1f34f134a153e1745cf849aedde1db17e4c4dc3a
Greg Leeper <gleeper49@gmail.com>
2023-05-26 19:14:09 -0400

    d20 start

--- aoc/y2017/d20.go
+++ aoc/y2017/d20.go
@@ -3,6 +3,9 @@ package y2017
 import (
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
+	"github.com/leep-frog/euler_challenge/maths"
+	"github.com/leep-frog/euler_challenge/parse"
+	"github.com/leep-frog/euler_challenge/rgx"
 )
 
 func Day20() aoc.Day {
@@ -12,6 +15,14 @@ func Day20() aoc.Day {
 type day20 struct{}
 
 func (d *day20) Solve(lines []string, o command.Output) {
+	r := rgx.New(`^p=< *([-0-9]+), *([-0-9]+), *([-0-9]+)>, v=< *([-0-9]+), *([-0-9]+), *([-0-9]+)>, a=< *([-0-9]+), *([-0-9]+), *([-0-9]+)>$`)
+	best := maths.Smallest[int, int]()
+	for i, line := range lines {
+		m := r.MustMatch(line)
+		ax, ay, az := parse.Atoi(m[6]), parse.Atoi(m[7]), parse.Atoi(m[8])
+		best.IndexCheck(i, maths.Abs(ax)+maths.Abs(ay)+maths.Abs(az))
+	}
+	o.Stdoutln(best.BestIndex())
 }
 
 func (d *day20) Cases() []*aoc.Case {
