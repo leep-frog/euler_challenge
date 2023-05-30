56821b078259eeff453efab258bc4c31c038a58f
leep-frog <gleeper49@gmail.com>
2023-05-30 08:45:55 -0400

    stuff

--- aoc/y2018/d12.go
+++ aoc/y2018/d12.go
@@ -1,6 +1,8 @@
 package y2018
 
 import (
+	"strings"
+
 	"github.com/leep-frog/command"
 	"github.com/leep-frog/euler_challenge/aoc"
 )
@@ -11,7 +13,23 @@ func Day12() aoc.Day {
 
 type day12 struct{}
 
+type plantRule struct {
+	input  []bool
+	output bool
+}
+
 func (d *day12) Solve(lines []string, o command.Output) {
+	state := map[int]bool{}
+	for i, c := range strings.Split(strings.TrimPrefix(lines[0], "initial state: "), "") {
+		if c == "#" {
+			state[i] = true
+		}
+	}
+
+	for g := 0; g < 20; g++ {
+
+	}
+
 }
 
 func (d *day12) Cases() []*aoc.Case {
