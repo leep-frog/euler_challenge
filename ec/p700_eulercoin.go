67a5395ed29ba6ecee23db889ac410160d0de72b
Greg Leeper <gleeper49@gmail.com>
2024-07-10 17:44:48 -0400

    p700

---
+++ ec/p700_eulercoin.go
@@ -0,0 +1,51 @@
+package eulerchallenge
+
+import (
+	"github.com/leep-frog/command/command"
+	"github.com/leep-frog/euler_challenge/maths"
+)
+
+func P700() *problem {
+	return noInputNode(700, func(o command.Output) {
+		incr := uint64(1504170715041707)
+		mod := uint64(4503599627370517)
+		var sum uint64
+
+		best := maths.Smallest[int, uint64]()
+
+		for at := incr; !best.Set() || best.Best() > 1; at = (at + incr) % mod {
+			prevBest := best.Best()
+
+			if best.Check(at) {
+				sum += at
+
+				// Based on how modulus works, the difference will be the same every
+				// k iterations (and will guaranteed not get lower before that)
+				// so just automatically jump down by that amount
+				if prevBest != 0 {
+					diff := prevBest - at
+					for at > diff {
+						at -= diff
+						best.Check(at)
+						sum += at
+					}
+				}
+
+				// We don't need to increment redundantly above the current best (at),
+				// so lower the mod by incr
+				for mod > at+incr {
+					mod = mod - incr
+				}
+
+				// Also redundant if the increment is larger than the modulus, so imrpove that as well
+				if incr > mod {
+					incr = incr % mod
+				}
+			}
+		}
+
+		o.Stdoutln(sum)
+	}, &execution{
+		want: "1517926517777556",
+	})
+}
