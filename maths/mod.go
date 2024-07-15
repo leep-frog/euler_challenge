c2aad1be9988f8f476b343b486dcd05b604b21f5
Greg Leeper <gleeper49@gmail.com>
2024-07-15 15:22:07 -0400

    simplify SolveMod

--- maths/mod.go
+++ maths/mod.go
@@ -1,7 +1,10 @@
 package maths
 
+// SolveMod solves the following equation for x: a*x == target (mod mod)
+// This logic was determined from page 4 of https://www.math.utah.edu/~fguevara/ACCESS2013/Euclid.pdf
+//
+// NOTE: This method assumes that `a` and `mod` are co-prime!!
 func SolveMod(a, mod, target int) int {
-
 	if target == 0 {
 		return mod
 	}
@@ -10,15 +13,14 @@ func SolveMod(a, mod, target int) int {
 		return target
 	}
 
-	r := recurSolveMod(a, mod, "")[1]
+	r := recurSolveMod(a, mod)[1]
 	if r < 0 {
 		r += mod
 	}
 	return r * target % mod
 }
 
-func recurSolveMod(a, mod int, indent string) []int {
-
+func recurSolveMod(a, mod int) []int {
 	factor := mod / a
 	offset := mod % a
 
@@ -26,13 +28,9 @@ func recurSolveMod(a, mod int, indent string) []int {
 		return []int{1, -factor}
 	}
 
-	c := []int{1, -factor}
-	p := recurSolveMod(offset, a, indent+"  ")
-
-	r := []int{
-		c[0] * p[1],
-		p[0] + c[1]*p[1],
+	p := recurSolveMod(offset, a)
+	return []int{
+		p[1],
+		p[0] - factor*p[1],
 	}
-
-	return r
 }
