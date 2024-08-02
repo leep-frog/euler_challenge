12cb1efc5e34deb5d9b2c9e431dedcf363828ad1
Greg Leeper <gleeper49@gmail.com>
2024-08-02 16:52:24 -0400

    QUICKER

--- ec/p885/p885_sorted_digits.go
+++ ec/p885/p885_sorted_digits.go
@@ -1,8 +1,6 @@
 package p885
 
 import (
-	"fmt"
-
 	"github.com/leep-frog/command/command"
 	"github.com/leep-frog/euler_challenge/combinatorics"
 	"github.com/leep-frog/euler_challenge/ec/ecmodels"
@@ -13,10 +11,7 @@ const mod = 1123455689
 
 func P885() *ecmodels.Problem {
 	return ecmodels.IntInputNode(885, func(o command.Output, n int) {
-
-		for i := 1; i <= 18; i++ {
-			fmt.Println(i, combos(i, 0, nil))
-		}
+		o.Stdoutln(clever(n))
 	}, []*ecmodels.Execution{
 		{
 			Args: []string{"1"},
@@ -27,13 +22,25 @@ func P885() *ecmodels.Problem {
 			Want: "1543545675",
 		},
 		{
-			Args:     []string{"18"},
-			Want:     "827850196",
-			Estimate: 90,
+			Args: []string{"18"},
+			Want: "827850196",
+			// Estimate: 90, // My solution took 90s
 		},
 	})
 }
 
+// This is taken from the problem thread
+func clever(n int) int {
+	summation := maths.Zero()
+	for i := 1; i <= 9; i++ {
+		summation = summation.Plus(maths.BigPow(10+9*i, n))
+	}
+	summation = summation.DivInt(9)
+	summation = summation.Minus(maths.BigPow(10, n))
+	return summation.ModInt(mod)
+}
+
+// This is my OG solution (which would have been THIRD if started when initially released!!!)
 func combos(rem, min int, cur []int) int {
 	if rem == 0 {
 		return (combinatorics.PermutationCount(cur).ModInt(mod) * maths.IntFromDigits(cur).ModInt(mod)) % mod
