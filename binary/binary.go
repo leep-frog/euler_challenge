2ebc13d025d47d8a57102688cbd463426c4f3d4b
Greg Leeper <gleeper49@gmail.com>
2024-08-06 13:37:42 -0400

    binary test

--- binary/binary.go
+++ binary/binary.go
@@ -5,45 +5,66 @@ import (
 	"strings"
 
 	"github.com/leep-frog/euler_challenge/bread"
+	"github.com/leep-frog/functional"
 )
 
-// TODO: Binary package
+// Binary represents a number in binary
 type Binary struct {
 	// digits is ordered from left to right in order of decreasing significance.
 	// For example, [1, 0, 0, 1, 1] = 16 + 2 + 1 = 19
 	digits []bool
 }
 
+func (b *Binary) IsZero() bool {
+	return len(b.digits) == 0 || functional.All(b.digits, func(b bool) bool { return !b })
+}
+
+func (b *Binary) Copy() *Binary {
+	var c []bool
+	for _, d := range b.digits {
+		c = append(c, d)
+	}
+	return &Binary{c}
+}
+
+// Double the value of the binary bit
 func (b *Binary) Double() {
 	b.digits = append(b.digits, false)
-}
-
-func (b *Binary) Half() {
-	if len(b.digits) > 0 {
-		b.digits = b.digits[:len(b.digits)-1]
-	} else {
-		b.digits = []bool{false}
-	}
-}
-
-func (b *Binary) Size() int {
-	return len(b.digits)
 }
 
 func (b *Binary) DoublePlusOne() {
 	b.digits = append(b.digits, true)
 }
 
+func (b *Binary) Half() {
+	if len(b.digits) > 0 {
+		b.digits = b.digits[:len(b.digits)-1]
+	}
+}
+
+func (b *Binary) Size() int {
+	if b.IsZero() {
+		return 1
+	}
+	return len(b.digits)
+}
+
 func (b *Binary) String() string {
+	if b.IsZero() {
+		return "0"
+	}
+
 	var r []string
-	for i := 0; i < len(b.digits); i++ {
-		if b.At(i) {
+
+	for _, v := range b.digits {
+		if v {
 			r = append(r, "1")
 		} else {
 			r = append(r, "0")
 		}
 	}
-	return strings.Join(bread.Reverse(r), "")
+
+	return strings.Join(r, "")
 }
 
 func (b *Binary) ToInt() int {
@@ -74,7 +95,6 @@ func (b *Binary) ShortenedBinaryExpansion() string {
 	var counts []string
 	cur := true
 	startIndex := 0
-	//for i := 0; i < len(b.digits); i++ {
 	for i, v := range b.digits {
 		if v != cur {
 			counts = append(counts, fmt.Sprintf("%d", i-startIndex))
@@ -114,10 +134,15 @@ func (b *Binary) Minus(that *Binary) *Binary {
 			rem = true
 		}
 	}
-	// Trim the leading zeroes
-	for !digits[len(digits)-1] && len(digits) > 1 {
-		digits = digits[:len(digits)-1]
-	}
 
-	return &Binary{bread.Reverse(digits)}
+	nb := &Binary{bread.Reverse(digits)}
+	nb.trim()
+	return nb
+}
+
+// Trim the leading zeroes
+func (b *Binary) trim() {
+	for len(b.digits) > 0 && !b.digits[0] {
+		b.digits = b.digits[1:]
+	}
 }
--- binary/binary_test.go
+++ binary/binary_test.go
@@ -1,10 +1,11 @@
 package binary
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestBinary(t *testing.T) {
+func TestBinaryBinaryOps(t *testing.T) {
 	for _, test := range []struct {
 		name           string
 		a              int
@@ -65,6 +66,101 @@ func TestBinary(t *testing.T) {
 			}
 			if got := diffBin.String(); got != test.wantBinaryDiff {
 				t.Errorf("BinaryFromInt(%d).Minus(BinaryFromInt(%d).String()) returned %s; want %s", test.a, test.b, got, test.wantBinaryDiff)
+			}
+		})
+	}
+}
+
+func TestUnaryBinaryOps(t *testing.T) {
+	for _, test := range []struct {
+		name              string
+		b                 int
+		wantB             string
+		wantDouble        int
+		wantDoublePlusOne int
+		wantHalf          int
+		wantIsZero        bool
+	}{
+		{
+			name:              "Zero",
+			b:                 0,
+			wantB:             "0",
+			wantDouble:        0,
+			wantDoublePlusOne: 1,
+			wantHalf:          0,
+			wantIsZero:        true,
+		},
+		{
+			name:              "One",
+			b:                 1,
+			wantB:             "1",
+			wantDouble:        2,
+			wantDoublePlusOne: 3,
+			wantHalf:          0,
+		},
+		{
+			name:              "Two",
+			b:                 2,
+			wantB:             "10",
+			wantDouble:        4,
+			wantDoublePlusOne: 5,
+			wantHalf:          1,
+		},
+		{
+			name:              "Three",
+			b:                 3,
+			wantB:             "11",
+			wantDouble:        6,
+			wantDoublePlusOne: 7,
+			wantHalf:          1,
+		},
+		{
+			name:              "Four",
+			b:                 4,
+			wantB:             "100",
+			wantDouble:        8,
+			wantDoublePlusOne: 9,
+			wantHalf:          2,
+		},
+		{
+			name:              "Five",
+			b:                 5,
+			wantB:             "101",
+			wantDouble:        10,
+			wantDoublePlusOne: 11,
+			wantHalf:          2,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			fmt.Println("TESTING ==============", test.b)
+			bin := BinaryFromInt(test.b)
+			if got := bin.ToInt(); got != test.b {
+				t.Errorf("BinaryFromInt(%d).ToInt() returned %d", test.b, got)
+			}
+			if got := bin.String(); got != test.wantB {
+				t.Errorf("BinaryFromInt(%d).ToString() returned %s; want %s", test.b, got, test.wantB)
+			}
+
+			cp := bin.Copy()
+			cp.Double()
+			if cp.ToInt() != test.wantDouble {
+				t.Errorf("BinaryFromInt(%d).Double() returned %d; want %d", test.b, cp.ToInt(), test.wantDouble)
+			}
+
+			cp = bin.Copy()
+			cp.DoublePlusOne()
+			if cp.ToInt() != test.wantDoublePlusOne {
+				t.Errorf("BinaryFromInt(%d).DoublePlusOne() returned %d; want %d", test.b, cp.ToInt(), test.wantDoublePlusOne)
+			}
+
+			cp = bin.Copy()
+			cp.Half()
+			if cp.ToInt() != test.wantHalf {
+				t.Errorf("BinaryFromInt(%d).Half() returned %d; want %d", test.b, cp.ToInt(), test.wantHalf)
+			}
+
+			if bin.IsZero() != test.wantIsZero {
+				t.Errorf("BinaryFromInt(%d).IsZero() returned %v; want %v", test.b, bin.IsZero(), test.wantIsZero)
 			}
 		})
 	}
