30fd4107687474954a475c15e6a09be42e1c72c1
Greg Leeper <gleeper49@gmail.com>
2024-08-12 14:11:12 -0400

    improve 181

--- ec/p181/p181_investigating_in_how_many_ways_objects_of_two_different_colours_can_be_grouped.go
+++ ec/p181/p181_investigating_in_how_many_ways_objects_of_two_different_colours_can_be_grouped.go
@@ -5,8 +5,8 @@ import (
 	"github.com/leep-frog/euler_challenge/ec/ecmodels"
 )
 
-func rec(remB, remW, minB, minW int, cur [][]int, cache []int) int {
-	code := 1000000*remB + 10000*remW + 100*minB + minW
+func rec(remB, remW, minB, minW int, cur [][]int, cache []int, codeFunc func(b1, b2, w1, w2 int) int) int {
+	code := codeFunc(remB, minB, remW, minW)
 	if cache[code] >= 0 {
 		return cache[code]
 	}
@@ -28,13 +28,13 @@ func rec(remB, remW, minB, minW int, cur [][]int, cache []int) int {
 		if minB == 0 && w == 0 {
 			continue
 		}
-		sum += rec(remB-minB, remW-w, minB, w, append(cur, []int{minB, w}), cache)
+		sum += rec(remB-minB, remW-w, minB, w, append(cur, []int{minB, w}), cache, codeFunc)
 	}
 
 	// Now increase blacks
 	for b := minB + 1; b <= remB; b++ {
 		for w := 0; w <= remW; w++ {
-			sum += rec(remB-b, remW-w, b, w, append(cur, []int{b, w}), cache)
+			sum += rec(remB-b, remW-w, b, w, append(cur, []int{b, w}), cache, codeFunc)
 		}
 	}
 
@@ -46,21 +46,24 @@ func P181() *ecmodels.Problem {
 	return ecmodels.IntsInputNode(181, 2, 0, func(o command.Output, ns []int) {
 		b := ns[0]
 		w := ns[1]
-		c := make([]int, 100000000)
-		for i := range c {
-			c[i] = -1
-		}
-
 		if b > w {
 			b, w = w, b
 		}
 
-		o.Stdoutln(rec(w, b, 0, 0, nil, c))
+		c := make([]int, (b+1)*(b+1)*(w+1)*(w+1))
+		for i := range c {
+			c[i] = -1
+		}
+		codeFunc := func(b1, b2, w1, w2 int) int {
+			return (b+1)*(w+1)*(w+1)*b1 + (w+1)*(w+1)*b2 + (w+1)*w1 + w2
+		}
+
+		o.Stdoutln(rec(b, w, 0, 0, nil, c, codeFunc))
 	}, []*ecmodels.Execution{
 		{
-			Args: []string{"60", "40"},
-			Want: "83735848679360680",
-			// Estimate: 4*60 + 30,
+			Args:     []string{"60", "40"},
+			Want:     "83735848679360680",
+			Estimate: 15,
 		},
 	})
 }
