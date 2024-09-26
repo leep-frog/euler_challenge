11c110e9a12f74936fd3335d55c9e09833201a8a
Greg Leeper <gleeper49@gmail.com>
2024-09-26 12:51:18 -0400

    p822 works!!!!

--- ec/p822/p822_square_the_smallest.go
+++ ec/p822/p822_square_the_smallest.go
@@ -3,6 +3,7 @@ package p822
 import (
 	"fmt"
 	"math"
+	"math/big"
 
 	"github.com/leep-frog/command/command"
 	"github.com/leep-frog/euler_challenge/ec/ecmodels"
@@ -13,10 +14,20 @@ const (
 	mod = 1234567891
 )
 
+var (
+	log2 = math.Log(2)
+)
+
 type squareRepr struct {
-	k   int
-	pow int
-	log float64
+	k      int
+	twoPow int
+
+	// The number will be k^(2^twoPow), we take the double log of that:
+	// log(k^2^twoPow) = 2 ^ twoPow * log(k)
+	// log(log(k^2^twoPow)) = log(2 ^ twoPow * log(k))
+	//                      = log(2^twoPow) + log(log(k))
+	//                      = twoPow * log(2) + log(log(k))
+	loglogk float64
 }
 
 func (sr *squareRepr) increment() {
@@ -24,18 +35,51 @@ func (sr *squareRepr) increment() {
 }
 
 func (sr *squareRepr) incrementTimes(n int) {
-	pow := maths.Pow(2, n)
-	sr.log *= float64(pow)
-	sr.pow *= pow
+	sr.twoPow += n
 }
 
-func (sr *squareRepr) incrementUpTo(max *squareRepr) int {
+func (sr *squareRepr) incrementUpTo(rootMax *squareRepr) int {
 	var increments int
-	for 2*sr.log < max.log {
+	for sr.lt(rootMax) {
 		sr.increment()
 		increments++
 	}
 	return increments
+}
+
+const PRECISION = 100
+
+func newFloat(f float64) *big.Float {
+	r := new(big.Float)
+	r.SetPrec(PRECISION)
+	return r.SetFloat64(f)
+}
+
+func newFlint(i int) *big.Float {
+	r := new(big.Float)
+	r.SetPrec(PRECISION)
+	return r.SetInt64(int64(i))
+}
+
+func (sr *squareRepr) lt(that *squareRepr) bool {
+	ltp := newFlint(sr.twoPow)
+	ltp = ltp.Mul(ltp, newFloat(log2))
+	ltp = ltp.Add(ltp, newFloat(sr.loglogk))
+
+	rtp := newFlint(that.twoPow)
+	rtp = rtp.Mul(rtp, newFloat(log2))
+	rtp = rtp.Add(rtp, newFloat(that.loglogk))
+
+	return ltp.Cmp(rtp) <= 0
+
+	lf := float64(sr.twoPow)*log2 + sr.loglogk
+	left := big.NewFloat(lf)
+	rf := float64(that.twoPow)*log2 + that.loglogk
+	right := big.NewFloat(rf)
+
+	fmt.Println("LEFT", ltp, left, "RIGHT", rtp, right)
+
+	return left.Cmp(right) < 0
 }
 
 func P822() *ecmodels.Problem {
@@ -46,11 +90,16 @@ func P822() *ecmodels.Problem {
 		var srs []*squareRepr
 		for i := 2; i <= n; i++ {
 			fmt.Println("adding", i)
-			srs = append(srs, &squareRepr{i, 1, math.Log(float64(i))})
+			srs = append(srs, &squareRepr{i, 0, math.Log(math.Log(float64(i)))})
 		}
 
+		sqrtIdx := (maths.Sqrt(n)) - 1
+		sqrtSq := srs[sqrtIdx]
+		maxRoot := &squareRepr{sqrtSq.k, 0, sqrtSq.loglogk}
+		_ = maxRoot
+
 		// fmt.Println(brute(srs, m))
-		fmt.Println(elegant(srs, m))
+		fmt.Println(elegant(srs, m, maxRoot))
 
 		// First, get all values to be greater than the
 		// o.Stdoutln(n)
@@ -66,13 +115,13 @@ func P822() *ecmodels.Problem {
 	})
 }
 
-func elegant(nums []*squareRepr, times int) int {
+func elegant(nums []*squareRepr, times int, maxRoot *squareRepr) int {
 	var increments int
 	for _, n := range nums {
-		increments += n.incrementUpTo(nums[len(nums)-1])
+		increments += n.incrementUpTo(maxRoot)
 	}
 
-	incrAll := (times - increments) / len(nums)
+	incrAll := ((times - increments) / len(nums)) - 1
 	for _, n := range nums {
 		fmt.Println("DOING", n, incrAll)
 		n.incrementTimes(incrAll)
@@ -81,12 +130,12 @@ func elegant(nums []*squareRepr, times int) int {
 		// }
 	}
 
-	return brute(nums, (times-increments)%len(nums))
+	return brute(nums, ((times-increments)%len(nums))+len(nums))
 }
 
 func brute(nums []*squareRepr, times int) int {
 	heap := maths.NewHeap(func(sr1, sr2 *squareRepr) bool {
-		return sr1.log < sr2.log
+		return sr1.lt(sr2)
 	})
 
 	for _, n := range nums {
@@ -94,17 +143,26 @@ func brute(nums []*squareRepr, times int) int {
 	}
 
 	for i := 0; i < times; i++ {
+		// fmt.Println("start incrementing")
 		sr := heap.Pop()
 		sr.increment()
-		fmt.Println("incrementing", sr.k)
+		// fmt.Println("incrementing", sr.k)
 		heap.Push(sr)
+		// fmt.Println("done incrementing")
 	}
 
-	var sum int
+	var sum, cnt int
 	heap.Iter(func(sr *squareRepr) bool {
-		sum = (sum + maths.PowMod(sr.k, sr.pow, mod)) % mod
-		fmt.Println("summing", sr.k, sr.pow)
+		fmt.Println("summing", sr.k, sr.twoPow)
+		// sum = (sum + maths.PowMod(sr.k, maths.PowMod(2, sr.twoPow, mod), mod)) % mod
+
+		// Num twos is sr.twoPos
+		// numTwos := sr.twoPow % (mod - 1)
+
+		sum = (sum + maths.PowMod(sr.k, maths.PowMod(2, sr.twoPow, mod-1), mod)) % mod
+		cnt += sr.twoPow
 		return true
 	})
+	fmt.Println("COUNT", cnt)
 	return sum
 }
