a4d31a86950464675810cc49ff151ba90c05f6c0
Greg Leeper <gleeper49@gmail.com>
2024-09-30 12:07:15 -0400

    comment

--- ec/p874/p874_maximal_primal_score.go
+++ ec/p874/p874_maximal_primal_score.go
@@ -24,7 +24,7 @@ func P874() *ecmodels.Problem {
 
 		negativeSpace := max % n
 
-		o.Stdoutln(dp2(negativeSpace, negativeSpace, prime, n, 0))
+		o.Stdoutln(dp(negativeSpace, negativeSpace, prime, n, 0))
 
 	}, []*ecmodels.Execution{
 		{
@@ -43,7 +43,18 @@ func P874() *ecmodels.Problem {
 	})
 }
 
-func dp2(negativeSpace, at, numbersRemaining, n, currentScore int) int {
+// The solution does a two-fold approach:
+//  1. dynamic programming to speed things up (this builds the sum from bottom up).
+//     However, this only checks values when we reach another root node which can take a while.
+//  2. building the currentScore sum from top down to print out the current best at each
+//     leaf (to get around the problem from part 1.
+//
+// Based on the thread, we can also do the knapsack solution or check for optimal decrements (-4, and -9),
+// but the way I solved it is fine enough.
+//
+// If we find more uses for a generic knapsack solver, I can implement that,
+// otherwise, leave as is.
+func dp(negativeSpace, at, numbersRemaining, n, currentScore int) int {
 	code := fmt.Sprintf("%d-%d-%d", negativeSpace, at, numbersRemaining)
 	if v, ok := cache[code]; ok {
 		if n == 7000 && absBest.Check(currentScore+v) {
@@ -65,11 +76,11 @@ func dp2(negativeSpace, at, numbersRemaining, n, currentScore int) int {
 
 	// Check without adding another of these
 	if at > 1 {
-		best.Check(dp2(negativeSpace, at-1, numbersRemaining, n, currentScore))
+		best.Check(dp(negativeSpace, at-1, numbersRemaining, n, currentScore))
 	}
 
 	// Check with adding one of these
-	best.Check(p.Nth(n-1-at) + dp2(negativeSpace-at, maths.Min(at, negativeSpace-at), numbersRemaining-1, n, currentScore+p.Nth(n-1-at)))
+	best.Check(p.Nth(n-1-at) + dp(negativeSpace-at, maths.Min(at, negativeSpace-at), numbersRemaining-1, n, currentScore+p.Nth(n-1-at)))
 
 	cache[code] = best.Best()
 	return best.Best()
