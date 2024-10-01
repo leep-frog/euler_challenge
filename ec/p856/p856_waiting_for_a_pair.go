a584b16d10ee33ad28c25c06c01b7432381af85c
Greg Leeper <gleeper49@gmail.com>
2024-10-01 19:58:01 -0400

    slice cache for p856

--- ec/p856/p856_waiting_for_a_pair.go
+++ ec/p856/p856_waiting_for_a_pair.go
@@ -7,7 +7,6 @@ import (
 	"github.com/leep-frog/command/command"
 	"github.com/leep-frog/euler_challenge/ec/ecmodels"
 	"github.com/leep-frog/euler_challenge/fraction"
-	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 )
 
@@ -16,7 +15,10 @@ func P856() *ecmodels.Problem {
 		suits := 4
 		values := 13
 
-		o.Stdoutf("%.8f\n", expectedValue(suits-1, map[int]int{suits: values - 1}, suits*values-1, suits*values).Float64())
+		counts := make([]int, suits+1)
+		counts[suits] = values - 1
+
+		o.Stdoutf("%.8f\n", expectedValue(suits-1, counts, suits*values-1, suits*values).Float64())
 	}, &ecmodels.Execution{
 		Want: "17.09661501",
 	})
@@ -26,17 +28,15 @@ var (
 	cache = map[string]*fraction.Rational{}
 )
 
-func expectedValue(prevCnt int, counts map[int]int, cardsLeft, totalCards int) *fraction.Rational {
+func expectedValue(prevCnt int, counts []int, cardsLeft, totalCards int) *fraction.Rational {
 	if cardsLeft == 0 {
 		return fraction.NewRational(totalCards, 1)
 	}
 
 	// Construct the code for the cache
-	keys := maps.Keys(counts)
-	slices.Sort(keys)
 	codeParts := []string{fmt.Sprintf("%d", prevCnt)}
-	for _, k := range keys {
-		codeParts = append(codeParts, fmt.Sprintf("%d:%d", k, counts[k]))
+	for i, v := range counts {
+		codeParts = append(codeParts, fmt.Sprintf("%d:%d", i, v))
 	}
 	code := strings.Join(codeParts, " ")
 
@@ -50,16 +50,17 @@ func expectedValue(prevCnt int, counts map[int]int, cardsLeft, totalCards int) *
 
 	// Iterate over all possible cards to draw
 	for numberOfCards, freq := range counts {
+		if numberOfCards == 0 || freq == 0 {
+			continue
+		}
+
 		// Probability a value with `numberOfCards` remaining was picked
 		curP := fraction.NewRational(numberOfCards*freq, cardsLeft)
-		newCounts := maps.Clone(counts)
+		newCounts := slices.Clone(counts)
 		if prevCnt > 0 {
 			newCounts[prevCnt]++
 		}
 		newCounts[numberOfCards]--
-		if newCounts[numberOfCards] == 0 {
-			delete(newCounts, numberOfCards)
-		}
 
 		// Add the expected value of this situation
 		ev = ev.Plus(curP.Times(expectedValue(numberOfCards-1, newCounts, cardsLeft-1, totalCards)))
