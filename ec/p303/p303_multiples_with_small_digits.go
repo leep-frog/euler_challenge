64d5ead00d4f562ff05318ef7fa9336f7f4bc549
Greg Leeper <gleeper49@gmail.com>
2025-01-23 14:56:38 -0500

    p303 edits

--- ec/p303/p303_multiples_with_small_digits.go
+++ ec/p303/p303_multiples_with_small_digits.go
@@ -16,58 +16,53 @@ func P303() *ecmodels.Problem {
 		for i := 1; i <= n; i++ {
 			m[i] = true
 		}
-		fmt.Println(dfs(m, []*maths.Int{maths.NewInt(1), maths.NewInt(2)}, n))
+		o.Stdoutln(dfs(m, []*maths.Int{maths.NewInt(1), maths.NewInt(2)}, n))
 	}, []*ecmodels.Execution{
 		{
 			Args: []string{"2"},
-			Want: "",
+			Want: "11363107",
+		},
+		{
+			Args: []string{"4"},
+			Want: "1111981904675169",
 		},
 	})
 }
 
-// func markCompleted(m map[int]bool, n, k int, v *maths.Int) *maths.Int {
-// 	sum := maths.Zero()
+func markCompleted(m map[int]bool, n, k int, v *maths.Int) *maths.Int {
+	sum := maths.Zero()
 
-// 	for cur := k; cur <= n; cur *= 10 {
-// 		delete(m, cur)
-// 		sum = sum.Plus(v)
-// 	}
-// }
+	for cur := k; cur <= n; cur *= 10 {
+		delete(m, cur)
+		// This intentionally uses k because the ratio stays the same:
+		// (v / k) == (10v / 10k) == (100v / 100k) == ...
+		sum = sum.Plus(v.DivInt(k))
+	}
+	return sum
+}
 
 func dfs(m map[int]bool, opts []*maths.Int, n int) *maths.Int {
 
 	sum := maths.Zero()
 
-	// First, clear all the /9+/ values because they are the largest
-
-	// for
+	// First, clear all the /9+/ values because they require the largest values
+	// but create a very simple pattern (k ones followed by 4k twos)
+	for nines, ones, twos := 9, []int{1}, []int{2, 2, 2, 2}; nines <= n; nines, ones, twos = nines*10+9, append(ones, 1), append(twos, 2, 2, 2, 2) {
+		sum = sum.Plus(markCompleted(m, n, nines, maths.IntFromDigits(append(ones, twos...))))
+	}
 
 	for len(m) > 0 {
 		next := opts[0]
 		opts = opts[1:]
-		// fmt.Println(next)
-
-		// maps
-		// 9 12222
-		// 99 1122222222
-		// 999 111222222222222
-		// 9999 11112222222222222222
-		// 122212222222221
-		// 11112222222222222222
 
 		for _, k := range maps.Keys(m) {
 			if next.ModInt(k) == 0 {
-				delete(m, k)
-				sum = sum.Plus(next.DivInt(k))
+
+				sum = sum.Plus(markCompleted(m, n, k, next))
 				fmt.Println(len(m))
 
 				if k == 9 || k == 99 || k == 999 || k == 9999 {
 					fmt.Println("nines", k, next)
-				}
-
-				for mult := k * 10; mult <= n; mult *= 10 {
-					delete(m, mult)
-					sum = sum.Plus(next.DivInt(k))
 				}
 
 				if len(m) < 20 {
